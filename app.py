15839a10dc3feb263aa037fe499f363f7ede1031
amol londhe <amolbrand00@gmail.com>
2024-01-11 17:55:38 +0530

    hn

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template
+from flask_cors import CORS
 from flask_socketio import SocketIO, emit
 import requests
 
 app = Flask(__name__)
+CORS(app)
 socketio = SocketIO(app)
 
 # External chatbot API link
